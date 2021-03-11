e9515713f15d83b79dd73f16308f7670e8a503d9
TamaraIstrati <tamara.istrati2000@gmail.com>
2021-03-11 18:05:04 +0200

    Metoda citesteAplicant partajata.

--- CTS_Seminar21/src/ro/ase/cts/clase/IReader.java
+++ CTS_Seminar21/src/ro/ase/cts/clase/IReader.java
@@ -2,6 +2,7 @@ package ro.ase.cts.clase;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class IReader {
 	protected String filePath;
@@ -12,4 +13,20 @@ public abstract class IReader {
 		this.filePath = filePath;
 	}
 
+	public void citireAplicant(Scanner input, Aplicant aplicant) {
+		String nume = input.next();
+		String prenume = input.next();
+		int varsta = input.nextInt();
+		int punctaj = input.nextInt();
+		int nr = input.nextInt();
+		String[] vect = new String[nr];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input.next();
+		
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setPunctaj(punctaj);
+		aplicant.setVectorDenumiri(vect, nr);
+	}
 }
