4ccf0e8ae5ac3d4ae16b354418084c30d4875ef1
TamaraIstrati <tamara.istrati2000@gmail.com>
2021-04-01 18:32:21 +0300

    Builder

---
+++ CTS_Seminar6/src/ro/ase/cts/builder/clase/RezervareBuilder.java
@@ -0,0 +1,53 @@
+package ro.ase.cts.builder.clase;
+
+public class RezervareBuilder implements AbstractBuilder{
+	private Rezervare rezervare;
+	
+
+	public RezervareBuilder() {
+		super();
+		this.rezervare = new Rezervare(0, false, false, false, false, "undef");
+	}
+	
+	public RezervareBuilder(int codRezervare) {
+		super();
+		this.rezervare = new Rezervare(codRezervare, false, false, false, false, "");
+	}
+
+	public RezervareBuilder setCodRezervare(int codRezervare) {
+		rezervare.setCodRezervare(codRezervare);
+		return this;
+	}
+	
+	public RezervareBuilder setAreMancare(boolean areMancare) {
+		rezervare.setAreMancare(areMancare);
+		return this;
+	}
+	
+	public RezervareBuilder setAreScaun(boolean areScaun) {
+		rezervare.setAreScaunErgonomic(areScaun);
+		return this;
+	}
+	
+	public RezervareBuilder setAreBautura(boolean areBautura) {
+		rezervare.setAreBautura(areBautura);;;
+		return this;
+	}
+	
+	public RezervareBuilder setAreMuzica(boolean areMuzica) {
+		rezervare.setAreMuzicaAmbientala(areMuzica);
+		return this;
+	}
+	
+	public RezervareBuilder setGenMuzica(String gen) {
+		rezervare.setGenMuzica(gen);
+		return this;
+	}
+	
+	@Override
+	public Rezervare build() {
+		return rezervare;
+	}
+
+	
+}
---
+++ CTS_Seminar6/src/ro/ase/cts/builder/main/Main.java
@@ -0,0 +1,38 @@
+package ro.ase.cts.builder.main;
+
+import ro.ase.cts.builder.clase.Rezervare;
+import ro.ase.cts.builder.clase.RezervareBuilder;
+import ro.ase.cts.builder.clase.RezervareBuilderV2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		///Met 1
+		Rezervare rezervare1;
+		Rezervare rezervare2;
+		
+		RezervareBuilder rezervareBuilder = new RezervareBuilder();
+		rezervareBuilder.setCodRezervare(100).setAreMancare(true);
+		rezervare1 = rezervareBuilder.build();
+		
+		rezervare2 = new RezervareBuilder(101).setAreMuzica(true).build();
+		
+		Rezervare rezervare3 = new Rezervare(102, true, false, true, true, "pop");
+		
+		Rezervare rezervare4 = rezervareBuilder.setCodRezervare(104).setAreMuzica(true).setAreScaun(true).build();;
+		
+		System.out.println(rezervare1);
+		System.out.println(rezervare2);
+		System.out.println(rezervare3);
+		System.out.println(rezervare4);
+		
+		///Met 2
+		RezervareBuilderV2 rbv2 = new RezervareBuilderV2().setAreMancare(true).setAreBautura(true).setAreScaunErgonomic(true);
+		Rezervare rezervare5 = rbv2.setCodRezervare(140).build();
+		Rezervare rezervare6 = rbv2.setCodRezervare(150).build();
+		
+		System.out.println(rezervare5);
+		System.out.println(rezervare6);
+	}
+
+}
