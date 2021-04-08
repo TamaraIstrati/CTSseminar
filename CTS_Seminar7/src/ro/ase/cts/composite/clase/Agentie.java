a94c5f93fb83b371b65392f937fff250d6f19cea
TamaraIstrati <tamara.istrati2000@gmail.com>
2021-04-08 19:04:44 +0300

    Composite

---
+++ CTS_Seminar7/src/ro/ase/cts/composite/clase/Agentie.java
@@ -0,0 +1,46 @@
+package ro.ase.cts.composite.clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agentie implements ISediu{
+	private List<ISediu> listaSedii;
+
+	public Agentie() {
+		super();
+		this.listaSedii = new ArrayList<>();
+	}
+	
+	public Agentie(List<ISediu> listaSedii) {
+		super();
+		this.listaSedii = listaSedii;
+	}
+
+	@Override
+	public void afisareDescriere() {
+		// TODO Auto-generated method stub
+		System.out.println("Este o agentie.");
+		for(ISediu s : listaSedii){
+			s.afisareDescriere();
+		}
+	}
+
+	@Override
+	public void adaugaNod(ISediu sediu) {
+		// TODO Auto-generated method stub
+		listaSedii.add(sediu);
+	}
+
+	@Override
+	public void stergeNod(ISediu sediu) {
+		// TODO Auto-generated method stub
+		listaSedii.remove(sediu);
+	}
+
+	@Override
+	public ISediu getNod(int pos) {
+		// TODO Auto-generated method stub
+		return listaSedii.get(pos);
+	}
+
+}
