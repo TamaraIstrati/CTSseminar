d6d468642ac72e790461b809e59c1f28781e086e
TamaraIstrati <tamara.istrati2000@gmail.com>
2021-05-28 15:29:19 +0300

    right bicep si correct

---
+++ CTS_Seminar13/src/ro/ase/cts/testareUnitara/teste/TesteGrupaFixture.java
@@ -0,0 +1,36 @@
+package ro.ase.cts.testareUnitara.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.ase.cts.testareUnitara.clase.Grupa;
+import ro.ase.cts.testareUnitara.clase.Student;
+
+public class TesteGrupaFixture {
+	private Grupa grupa;
+
+	@Before
+	public void setup() {
+		grupa = new Grupa(1081);
+		for(int i=0; i<20; i++) {
+			Student s = new Student("Student"+i);
+			s.adaugaNota(10);
+			s.adaugaNota(7);
+			grupa.adaugaStudent(s);
+		}
+		for(int i=0; i<15; i++) {
+			Student s = new Student("Stud"+i);
+			s.adaugaNota(4);
+			s.adaugaNota(7);
+			grupa.adaugaStudent(s);
+		}
+	}
+	
+	@Test(timeout = 500)
+	public void testGetPromovabilitatePerformance() {
+		grupa.getPromovabilitate();
+	}
+
+}
