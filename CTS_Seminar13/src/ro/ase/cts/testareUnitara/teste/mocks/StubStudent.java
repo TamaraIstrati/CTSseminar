c87395722935e8db8c2e9c59cc497d7c78beeb07
TamaraIstrati <tamara.istrati2000@gmail.com>
2021-06-03 18:29:56 +0300

    Dummy si Fake Class

---
+++ CTS_Seminar13/src/ro/ase/cts/testareUnitara/teste/mocks/StubStudent.java
@@ -0,0 +1,43 @@
+package ro.ase.cts.testareUnitara.teste.mocks;
+
+import java.util.List;
+
+import ro.ase.cts.testareUnitara.clase.IStudent;
+
+public class StubStudent implements IStudent{
+
+	@Override
+	public String getNume() {
+		return "Gigel";
+	}
+
+	@Override
+	public void setNume(String nume) {
+	}
+
+	@Override
+	public List<Integer> getNote() {
+		return null;
+	}
+
+	@Override
+	public void adaugaNota(int nota) {
+		
+	}
+
+	@Override
+	public float calculeazaMedie() {
+		return 6.7f;
+	}
+
+	@Override
+	public int getNota(int index) {
+		return 9;
+	}
+
+	@Override
+	public boolean areRestante() {
+		return false;
+	}
+
+}
